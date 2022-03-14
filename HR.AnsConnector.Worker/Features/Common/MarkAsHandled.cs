475366770deda4b38dcf8a48bf4d84cba656dbc6
jimatas <atask@hr.nl>
2022-03-14 12:25:25 +0100

    Mark as handled. Log statement.

---
+++ HR.AnsConnector.Worker/Features/Common/MarkAsHandled.cs
@@ -0,0 +1,33 @@
+﻿using Developist.Core.Cqrs.Commands;
+
+namespace HR.AnsConnector.Features.Common
+{
+    public class MarkAsHandled : ICommand
+    {
+        /// <summary>
+        /// The generated id of the element in Ans.
+        /// </summary>
+        public int? Id { get; set; }
+        public bool Success { get; set; }
+        public int? EventId { get; set; }
+
+        /// <summary>
+        /// The HTTP status code and message of the API response. 
+        /// </summary>
+        public string? StatusInfo { get; set; }
+
+        /// <summary>
+        /// Any validation errors that were returned by the server; flattened to a single error message.
+        /// </summary>
+        public string? ErrorMessage { get; set; }
+    }
+
+    public class MarkAsHandledHandler : ICommandHandler<MarkAsHandled>
+    {
+
+        public Task HandleAsync(MarkAsHandled command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HR.AnsConnector.Worker/Infrastructure/ApiResponseExtensions.cs
+++ HR.AnsConnector.Worker/Infrastructure/ApiResponseExtensions.cs
@@ -2,6 +2,12 @@
 {
     public static class ApiResponseExtensions
     {
+        /// <summary>
+        /// Produces a single flattened error message given the validation errors, if any, that were returned by the server.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="apiResponse"></param>
+        /// <returns></returns>
         public static string GetValidationErrorsAsSingleMessage<T>(this ApiResponse<T> apiResponse)
             => string.Join(Environment.NewLine, apiResponse.ValidationErrors.Select(error => $"{error.Key}: {string.Join(", ", error.Value)}"));
     }
