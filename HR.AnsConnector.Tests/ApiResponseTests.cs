6295e9878294abbcad1393b4d1db04f87d9b5d4f
jimatas <atask@hr.nl>
2022-03-15 19:48:48 +0100

    Add unit tests.

---
+++ HR.AnsConnector.Tests/ApiResponseTests.cs
@@ -0,0 +1,36 @@
+﻿using HR.AnsConnector.Infrastructure;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HR.AnsConnector.Tests
+{
+    [TestClass]
+    public class ApiResponseTests
+    {
+        [TestMethod]
+        public void GetStatusMessage_ByDefault_ReturnsExpectedString()
+        {
+            // Arrange
+            var apiResponse = new ApiResponse();
+
+            // Act
+            var result = apiResponse.GetStatusMessage();
+
+            // Assert
+            Assert.AreEqual("HTTP  - ", result);
+        }
+
+        [TestMethod]
+        public void GetStatusMessage_WithStatusCodeAndDescriptionSet_ReturnsExpectedString()
+        {
+            // Arrange
+            var apiResponse = new ApiResponse { StatusCode = 200, StatusDescription = "OK" };
+
+            // Act
+            var result = apiResponse.GetStatusMessage();
+
+            // Assert
+            Assert.AreEqual("HTTP 200 - OK", result);
+        }
+    }
+}
