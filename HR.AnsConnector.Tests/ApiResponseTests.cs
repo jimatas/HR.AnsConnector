e08c6b7a5f3e3fd616a7554010f19220f4e9e1a5
jimatas <atask@hr.nl>
2022-03-15 20:11:08 +0100

    Added some more unit tests.

--- HR.AnsConnector.Tests/ApiResponseTests.cs
+++ HR.AnsConnector.Tests/ApiResponseTests.cs
@@ -1,6 +1,9 @@
-﻿using HR.AnsConnector.Infrastructure;
+﻿using HR.AnsConnector.Features.Users;
+using HR.AnsConnector.Infrastructure;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
 
 namespace HR.AnsConnector.Tests
 {
@@ -31,6 +34,77 @@ namespace HR.AnsConnector.Tests
 
             // Assert
             Assert.AreEqual("HTTP 200 - OK", result);
+        }
+
+        [TestMethod]
+        public void GetValidationErrorsAsSingleMessage_ByDefault_ReturnsEmptyString()
+        {
+            // Arrange
+            var apiResponse = new ApiResponse<User>();
+
+            // Act
+            var result = apiResponse.GetValidationErrorsAsSingleMessage();
+
+            // Assert
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void GetValidationErrorsAsSingleMessage_GivenSingleError_ReturnsExpectedString()
+        {
+            // Arrange
+            var apiResponse = new ApiResponse<User>
+            {
+                ValidationErrors = new Dictionary<string, IEnumerable<string>>
+                {
+                    { "email", new[] { "is required" } }
+                }
+            };
+
+            // Act
+            var result = apiResponse.GetValidationErrorsAsSingleMessage();
+
+            // Assert
+            Assert.AreEqual("email: is required", result);
+        }
+
+        [TestMethod]
+        public void GetValidationErrorsAsSingleMessage_GivenMultipleErrorsForSameField_ReturnsExpectedString()
+        {
+            // Arrange
+            var apiResponse = new ApiResponse<User>
+            {
+                ValidationErrors = new Dictionary<string, IEnumerable<string>>
+                {
+                    { "email", new[] { "is required", "Length must be 7 characters" } }
+                }
+            };
+
+            // Act
+            var result = apiResponse.GetValidationErrorsAsSingleMessage();
+
+            // Assert
+            Assert.AreEqual("email: is required\r\nemail: Length must be 7 characters", result);
+        }
+
+        [TestMethod]
+        public void GetValidationErrorsAsSingleMessage_GivenSingleErrorForDifferentFields_ReturnsExpectedString()
+        {
+            // Arrange
+            var apiResponse = new ApiResponse<User>
+            {
+                ValidationErrors = new Dictionary<string, IEnumerable<string>>
+                {
+                    { "email", new[] { "is required" } },
+                    { "student_number", new[] { "is required" } }
+                }
+            };
+
+            // Act
+            var result = apiResponse.GetValidationErrorsAsSingleMessage();
+
+            // Assert
+            Assert.AreEqual("email: is required\r\nstudent_number: is required", result);
         }
     }
 }
