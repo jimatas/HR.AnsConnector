d01a6d69d50d9508c6558317cbfd279c8f9fa244
jimatas <atask@hr.nl>
2022-03-15 10:17:11 +0100

    Further implemented mark as handled.

--- HR.AnsConnector.Worker/Features/Common/MarkAsHandled.cs
+++ HR.AnsConnector.Worker/Features/Common/MarkAsHandled.cs
@@ -4,22 +4,32 @@ namespace HR.AnsConnector.Features.Common
 {
     public class MarkAsHandled : ICommand
     {
-        /// <summary>
-        /// The generated id of the element in Ans.
-        /// </summary>
-        public int? Id { get; set; }
-        public bool Success { get; set; }
-        public int? EventId { get; set; }
+        public MarkAsHandled(bool success, string statusMessage, string? errorMessage, int? id, int? eventId)
+        {
+            Success = success;
+            StatusMessage = statusMessage;
+            ErrorMessage = errorMessage;
+            Id = id;
+            EventId = eventId;
+        }
+
+        public bool Success { get; }
 
         /// <summary>
-        /// The HTTP status code and description of the response. 
+        /// The HTTP status code and description of the server response. 
         /// </summary>
-        public string? StatusMessage { get; set; }
+        public string StatusMessage { get; }
 
         /// <summary>
         /// Any validation errors that were returned by the server; flattened to a single error message.
         /// </summary>
-        public string? ErrorMessage { get; set; }
+        public string? ErrorMessage { get; }
+
+        /// <summary>
+        /// The generated id of the element in Ans, if successful.
+        /// </summary>
+        public int? Id { get; }
+        public int? EventId { get; }
     }
 
     public class MarkAsHandledHandler : ICommandHandler<MarkAsHandled>
