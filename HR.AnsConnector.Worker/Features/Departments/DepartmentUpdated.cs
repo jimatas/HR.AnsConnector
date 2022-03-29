f75d11ea09926a7674838f7a5d6dbe7416870cd2
jimatas <atask@hr.nl>
2022-03-29 16:08:21 +0200

    Implemented UpdateDepartment.

---
+++ HR.AnsConnector.Worker/Features/Departments/DepartmentUpdated.cs
@@ -0,0 +1,58 @@
+﻿using HR.AnsConnector.Features.Common;
+using HR.AnsConnector.Infrastructure;
+using HR.Common.Cqrs.Commands;
+using HR.Common.Cqrs.Events;
+using HR.Common.Utilities;
+
+namespace HR.AnsConnector.Features.Departments
+{
+    public class DepartmentUpdated : IEvent
+    {
+        public DepartmentUpdated(DepartmentRecord department, ApiResponse<Department> apiResponse)
+        {
+            StatusMessage = apiResponse.GetStatusMessage();
+            Success = apiResponse.IsSuccessStatusCode();
+            if (Success)
+            {
+                DepartmentId = apiResponse.Data!.Id;
+            }
+            else
+            {
+                ErrorMessage = apiResponse.GetValidationErrorsAsSingleMessage();
+            }
+            EventId = department.EventId;
+        }
+
+        public bool Success { get; }
+        public string StatusMessage { get; }
+        public string? ErrorMessage { get; }
+        public int? DepartmentId { get; }
+        public int? EventId { get; }
+    }
+
+    public class DepartmentUpdatedHandler : IEventHandler<DepartmentUpdated>
+    {
+        private readonly ICommandDispatcher commandDispatcher;
+        private readonly ILogger logger;
+
+        public DepartmentUpdatedHandler(ICommandDispatcher commandDispatcher, ILogger<DepartmentUpdatedHandler> logger)
+        {
+            this.commandDispatcher = commandDispatcher;
+            this.logger = logger;
+        }
+
+        public async Task HandleAsync(DepartmentUpdated e, CancellationToken cancellationToken)
+        {
+            logger.LogDebug($"Handling {nameof(DepartmentUpdated)} event by dispatching {nameof(MarkAsHandled)} command.");
+
+            await commandDispatcher.DispatchAsync(
+                new MarkAsHandled(
+                    e.Success,
+                    e.StatusMessage,
+                    e.ErrorMessage,
+                    e.DepartmentId,
+                    e.EventId),
+                cancellationToken).WithoutCapturingContext();
+        }
+    }
+}
---
+++ HR.AnsConnector.Worker/Features/Departments/UpdateDepartment.cs
@@ -0,0 +1,57 @@
+﻿using HR.AnsConnector.Infrastructure;
+using HR.Common.Cqrs.Commands;
+using HR.Common.Cqrs.Events;
+using HR.Common.Utilities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.AnsConnector.Features.Departments
+{
+    public class UpdateDepartment : ICommand
+    {
+        public UpdateDepartment(DepartmentRecord department)
+        {
+            Department = department;
+        }
+
+        public DepartmentRecord Department { get; }
+    }
+
+    public class UpdateDepartmentHandler : ICommandHandler<UpdateDepartment>
+    {
+        private readonly IApiClient apiClient;
+        private readonly IEventDispatcher eventDispatcher;
+        private readonly ILogger logger;
+
+        public UpdateDepartmentHandler(IApiClient apiClient, IEventDispatcher eventDispatcher, ILogger<UpdateDepartmentHandler> logger)
+        {
+            this.apiClient = apiClient;
+            this.eventDispatcher = eventDispatcher;
+            this.logger = logger;
+        }
+
+        public async Task HandleAsync(UpdateDepartment command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Updating {Department} in Ans.", command.Department);
+
+            var apiResponse = await apiClient.UpdateDepartmentAsync(command.Department, cancellationToken).WithoutCapturingContext();
+            if (apiResponse.IsSuccessStatusCode())
+            {
+                logger.LogInformation("{Department} was successfully updated in Ans.", command.Department);
+            }
+            else if (apiResponse.IsErrorStatusCode())
+            {
+                logger.LogWarning("Received {StatusMessage} while attempting to update {Department} in Ans. [{ValidationErrors}]",
+                    apiResponse.GetStatusMessage(),
+                    command.Department,
+                    apiResponse.GetValidationErrorsAsSingleMessage());
+            }
+
+            await eventDispatcher.DispatchAsync(new DepartmentUpdated(command.Department, apiResponse), cancellationToken).WithoutCapturingContext();
+        }
+    }
+}
