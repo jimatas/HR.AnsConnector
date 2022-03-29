8589036808c8c836cfa37a6e3b1e5584df68ad2a
jimatas <atask@hr.nl>
2022-03-29 13:04:35 +0200

    Implemented CreateDepartment.

---
+++ HR.AnsConnector.Worker/Features/Departments/CreateDepartment.cs
@@ -0,0 +1,51 @@
+﻿using HR.AnsConnector.Infrastructure;
+using HR.Common.Cqrs.Commands;
+using HR.Common.Cqrs.Events;
+using HR.Common.Utilities;
+
+namespace HR.AnsConnector.Features.Departments
+{
+    public class CreateDepartment : ICommand
+    {
+        public CreateDepartment(DepartmentRecord department)
+        {
+            Department = department;
+        }
+
+        public DepartmentRecord Department { get; }
+    }
+
+    public class CreateDepartmentHandler : ICommandHandler<CreateDepartment>
+    {
+        private readonly IApiClient apiClient;
+        private readonly IEventDispatcher eventDispatcher;
+        private readonly ILogger logger;
+
+        public CreateDepartmentHandler(IApiClient apiClient, IEventDispatcher eventDispatcher, ILogger<CreateDepartmentHandler> logger)
+        {
+            this.apiClient = apiClient;
+            this.eventDispatcher = eventDispatcher;
+            this.logger = logger;
+        }
+
+        public async Task HandleAsync(CreateDepartment command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Creating {Department} in Ans.", command.Department);
+
+            var apiResponse = await apiClient.CreateDepartmentAsync(command.Department, cancellationToken).WithoutCapturingContext();
+            if (apiResponse.IsSuccessStatusCode())
+            {
+                logger.LogInformation("{Department} was successfully created in Ans.", command.Department);
+            }
+            else if (apiResponse.IsErrorStatusCode())
+            {
+                logger.LogWarning("Received {StatusMessage} while attempting to create {Department} in Ans. [{ValidationErrors}]",
+                    apiResponse.GetStatusMessage(),
+                    command.Department,
+                    apiResponse.GetValidationErrorsAsSingleMessage());
+            }
+
+            await eventDispatcher.DispatchAsync(new DepartmentCreated(command.Department, apiResponse), cancellationToken).WithoutCapturingContext();
+        }
+    }
+}
---
+++ HR.AnsConnector.Worker/Features/Departments/DepartmentCreated.cs
@@ -0,0 +1,58 @@
+﻿using HR.AnsConnector.Features.Common;
+using HR.AnsConnector.Infrastructure;
+using HR.Common.Cqrs.Commands;
+using HR.Common.Cqrs.Events;
+using HR.Common.Utilities;
+
+namespace HR.AnsConnector.Features.Departments
+{
+    public class DepartmentCreated : IEvent
+    {
+        public DepartmentCreated(DepartmentRecord department, ApiResponse<Department> apiResponse)
+        {
+            StatusMessage = apiResponse.GetStatusMessage();
+            Success = apiResponse.IsSuccessStatusCode();
+            if (Success)
+            {
+                DepartmentId = apiResponse.Data!.Id;
+            }
+            else
+            {
+                ErrorMessage = apiResponse.GetValidationErrorsAsSingleMessage();
+            }
+            EventId = department.EventId;
+        }
+
+        public bool Success { get; }
+        public string StatusMessage { get; }
+        public string? ErrorMessage { get; }
+        public int? DepartmentId { get; }
+        public int? EventId { get; }
+    }
+
+    public class DepartmentCreatedHandler : IEventHandler<DepartmentCreated>
+    {
+        private readonly ICommandDispatcher commandDispatcher;
+        private readonly ILogger logger;
+
+        public DepartmentCreatedHandler(ICommandDispatcher commandDispatcher, ILogger<DepartmentCreatedHandler> logger)
+        {
+            this.commandDispatcher = commandDispatcher;
+            this.logger = logger;
+        }
+
+        public async Task HandleAsync(DepartmentCreated e, CancellationToken cancellationToken)
+        {
+            logger.LogDebug($"Handling {nameof(DepartmentCreated)} event by dispatching {nameof(MarkAsHandled)} command.");
+
+            await commandDispatcher.DispatchAsync(
+                new MarkAsHandled(
+                    e.Success,
+                    e.StatusMessage,
+                    e.ErrorMessage,
+                    e.DepartmentId,
+                    e.EventId),
+                cancellationToken).WithoutCapturingContext();
+        }
+    }
+}
