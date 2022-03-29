86f90cc977028ab992842dc2f1f5e2e6542292ac
jimatas <atask@hr.nl>
2022-03-29 11:33:26 +0200

    Added a base class for integration tests and another integration test for the Users feature.

---
+++ HR.AnsConnector.Tests/IntegrationTestsBase.cs
@@ -0,0 +1,69 @@
+﻿using HR.AnsConnector.Infrastructure;
+
+using Microsoft.Extensions.Options;
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HR.AnsConnector.Tests
+{
+    public abstract class IntegrationTestsBase : IDisposable
+    {
+        private ApiSettings? apiSettings;
+        private HttpClient? httpClient;
+        private JsonSerializerOptions? jsonOptions;
+
+        protected IApiClient CreateApiClient(string baseUri = "https://stage.ans.app/api/v2/")
+        {
+            apiSettings ??= CreateApiSettings();
+            httpClient ??= CreateHttpClient(apiSettings);
+            jsonOptions ??= CreateJsonOptions();
+
+            IApiClient apiClient = new ApiClient(httpClient, Options.Create(apiSettings), Options.Create(jsonOptions));
+            return apiClient;
+
+            ApiSettings CreateApiSettings() => new()
+            {
+                BaseUri = baseUri,
+                BearerToken = Environment.GetEnvironmentVariable("ApiSettings:BearerToken", EnvironmentVariableTarget.User),
+                TenantId = int.TryParse(Environment.GetEnvironmentVariable("ApiSettings:TenantId", EnvironmentVariableTarget.User), out var tenantId) ? tenantId : null,
+            };
+
+            static JsonSerializerOptions CreateJsonOptions()
+            {
+                JsonSerializerOptions jsonOptions = new(JsonSerializerDefaults.Web)
+                {
+                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                    AllowTrailingCommas = true,
+                    DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+                };
+                jsonOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+
+                return jsonOptions;
+            }
+
+            static HttpClient CreateHttpClient(ApiSettings apiSettings)
+            {
+                var httpClient = new HttpClient
+                {
+                    BaseAddress = new Uri($"{apiSettings.BaseUri.TrimEnd('/')}/") // https://stackoverflow.com/a/23438417
+                };
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiSettings.BearerToken);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("HR.AnsConnector.Infrastructure.ApiClient", "1.0"));
+
+                return httpClient;
+            }
+        }
+
+        public void Dispose()
+        {
+            httpClient?.Dispose();
+            httpClient = null;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ HR.AnsConnector.Tests/UserTests.cs
@@ -0,0 +1,52 @@
+﻿using HR.AnsConnector.Features.Users;
+using HR.AnsConnector.Infrastructure;
+using HR.Common.Utilities;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Threading.Tasks;
+
+namespace HR.AnsConnector.Tests
+{
+    [TestClass]
+    public class UserTests : IntegrationTestsBase
+    {
+        [TestMethod]
+        public async Task CompleteLifecycleIntegrationTest()
+        {
+            IApiClient apiClient = CreateApiClient();
+
+            var user = new User
+            {
+                Email = "atask@hr.nl",
+                FirstName = "Jim",
+                LastName = "Atas",
+                UniqueId = "atask",
+                ExternalId = "atask@hro.nl",
+                Role = UserRole.Staff,
+            };
+
+            var apiResponse = await apiClient.CreateUserAsync(user).WithoutCapturingContext();
+            Assert.IsTrue(apiResponse.IsSuccessStatusCode());
+
+            user = apiResponse!;
+            Assert.IsNotNull(user.Id);
+            Assert.IsNotNull(user.CreatedAt);
+            // The following assertion might or might not hold true, as users are only soft-deleted.
+            // Assert.AreEqual(user.CreatedAt, user.UpdatedAt);
+
+            user.FirstName = "Jimbo";
+
+            apiResponse = await apiClient.UpdateUserAsync(user).WithoutCapturingContext();
+            Assert.IsTrue(apiResponse.IsSuccessStatusCode());
+
+            user = apiResponse!;
+            Assert.AreEqual("Jimbo", user.FirstName);
+            Assert.IsNotNull(user.UpdatedAt);
+            Assert.AreNotEqual(user.UpdatedAt, user.CreatedAt);
+
+            apiResponse = await apiClient.DeleteUserAsync(user).WithoutCapturingContext();
+            Assert.IsTrue(apiResponse.IsSuccessStatusCode());
+        }
+    }
+}
