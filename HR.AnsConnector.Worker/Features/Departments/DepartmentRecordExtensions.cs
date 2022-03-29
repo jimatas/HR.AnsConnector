d84dc938ddbb6b6c2db5493641308123ef71a237
jimatas <atask@hr.nl>
2022-03-29 17:04:34 +0200

    Implemented ProcessDepartments.

---
+++ HR.AnsConnector.Worker/Features/Departments/DepartmentRecordExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.AnsConnector.Features.Departments
+{
+    public static class DepartmentRecordExtensions
+    {
+        public static bool IsToBeCreated(this DepartmentRecord d) => string.Equals("c", d.Action, StringComparison.OrdinalIgnoreCase);
+        public static bool IsToBeUpdated(this DepartmentRecord d) => string.Equals("u", d.Action, StringComparison.OrdinalIgnoreCase);
+        public static bool IsToBeDeleted(this DepartmentRecord d) => string.Equals("d", d.Action, StringComparison.OrdinalIgnoreCase);
+    }
+}
---
+++ HR.AnsConnector.Worker/Features/Departments/ProcessDepartments.cs
@@ -0,0 +1,70 @@
+﻿using HR.Common.Cqrs.Commands;
+using HR.Common.Cqrs.Queries;
+using HR.Common.Utilities;
+
+namespace HR.AnsConnector.Features.Departments
+{
+    public class ProcessDepartments : ICommand
+    {
+        public ProcessDepartments(bool isDeleteContext = false)
+        {
+            IsDeleteContext = isDeleteContext;
+        }
+
+        public bool IsDeleteContext { get; }
+    }
+
+    public class ProcessDepartmentsHandler : ICommandHandler<ProcessDepartments>
+    {
+        private readonly IQueryDispatcher queryDispatcher;
+        private readonly ICommandDispatcher commandDispatcher;
+        private readonly ILogger logger;
+
+        public ProcessDepartmentsHandler(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher, ILogger<ProcessDepartmentsHandler> logger)
+        {
+            this.queryDispatcher = queryDispatcher;
+            this.commandDispatcher = commandDispatcher;
+            this.logger = logger;
+        }
+
+        public async Task HandleAsync(ProcessDepartments command, CancellationToken cancellationToken)
+        {
+            int created = 0,
+                updated = 0,
+                deleted = 0;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var nextDepartment = await queryDispatcher.DispatchAsync(new GetNextDepartment(), cancellationToken).WithoutCapturingContext();
+                if (nextDepartment is null
+                    || ((nextDepartment.IsToBeCreated() || nextDepartment.IsToBeUpdated()) && command.IsDeleteContext)
+                    || (nextDepartment.IsToBeDeleted() && !command.IsDeleteContext))
+                {
+                    logger.LogInformation("No more departments to {Action}. Ending batch run.", command.IsDeleteContext ? "delete" : "create or update");
+                    break;
+                }
+
+                if (nextDepartment.IsToBeCreated())
+                {
+                    await commandDispatcher.DispatchAsync(new CreateDepartment(nextDepartment), cancellationToken).WithoutCapturingContext();
+                    created++;
+                }
+                else if (nextDepartment.IsToBeUpdated())
+                {
+                    await commandDispatcher.DispatchAsync(new UpdateDepartment(nextDepartment), cancellationToken).WithoutCapturingContext();
+                    updated++;
+                }
+                else if (nextDepartment.IsToBeDeleted())
+                {
+                    await commandDispatcher.DispatchAsync(new DeleteDepartment(nextDepartment), cancellationToken).WithoutCapturingContext();
+                    deleted++;
+                }
+                logger.LogInformation("Processed {Processed} department(s) in total.", created + updated + deleted);
+
+                logger.LogDebug("Created {Created} department(s).", created);
+                logger.LogDebug("Updated {Updated} department(s).", updated);
+                logger.LogDebug("Deleted {Deleted} department(s).", deleted);
+            }
+        }
+    }
+}
