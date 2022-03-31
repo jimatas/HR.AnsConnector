8ead481e56a3eda0ca262432074fd3fc2e89c8e1
jimatas <atask@hr.nl>
2022-03-31 13:46:48 +0200

    Run host without capturing sync context.

--- HR.AnsConnector.Worker/Program.cs
+++ HR.AnsConnector.Worker/Program.cs
@@ -1,7 +1,8 @@
 using HR.AnsConnector.Infrastructure.Hosting;
+using HR.Common.Utilities;
 
 await Host.CreateDefaultBuilder(args)
     .UseStartup<Startup>()
     .UseDefaultServiceProvider(options => options.ValidateScopes = false)
     .Build()
-    .RunAsync();
+    .RunAsync().WithoutCapturingContext();
