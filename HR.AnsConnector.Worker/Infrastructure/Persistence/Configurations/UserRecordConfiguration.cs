3afcc532bf1934a6974a4855e2c308a5a2eccdec
jimatas <atask@hr.nl>
2022-04-01 13:38:31 +0200

    Deleted unneeded discard.

--- HR.AnsConnector.Worker/Infrastructure/Persistence/Configurations/UserRecordConfiguration.cs
+++ HR.AnsConnector.Worker/Infrastructure/Persistence/Configurations/UserRecordConfiguration.cs
@@ -26,8 +26,8 @@ namespace HR.AnsConnector.Infrastructure.Persistence.Configurations
                 .Ignore(u => u.RoleId)
                 .Ignore(u => u.DepartmentIds)
                 .Ignore(u => u.IsAlumni);
-
-            _ = builder.Property(u => u.Role).HasConversion(
+            
+            builder.Property(u => u.Role).HasConversion(
                 convertToProviderExpression: (UserRole? role) => null, // No need to map back to provider type.
                 convertFromProviderExpression: (string? role) => !string.IsNullOrEmpty(role) && roleMappings.ContainsKey(role) ? roleMappings[role] : null);
 
