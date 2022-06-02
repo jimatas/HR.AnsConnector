482af98937c88b9fe30d1768d75d576f14a86d9d
jimatas <atask@hr.nl>
2022-06-02 12:33:01 +0200

    Added StudyRecord class.

---
+++ HR.AnsConnector.Worker/Features/Studies/StudyRecord.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HR.AnsConnector.Features.Studies
+{
+    /// <summary>
+    /// Extends the <see cref="Study"/> class and adds some metadata properties.
+    /// </summary>
+    public class StudyRecord : Study
+    {
+        [JsonIgnore]
+        public int? EventId { get; set; }
+
+        /// <summary>
+        /// Specifies the type of action to perform:
+        /// <list type="table">
+        ///   <item>
+        ///     <term>"c"</term>
+        ///     <description>Create a new entry.</description>
+        ///   </item>
+        ///   <item>
+        ///     <term>"u"</term>
+        ///     <description>Update an existing entry.</description>
+        ///   </item>
+        ///   <item>
+        ///     <term>"d"</term>
+        ///     <description>(Soft-)delete an entry.</description>
+        ///   </item>
+        /// </list>
+        /// </summary>
+        [JsonIgnore]
+        public string? Action { get; set; }
+    }
+}
