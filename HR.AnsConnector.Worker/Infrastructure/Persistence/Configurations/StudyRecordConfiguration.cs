9f7ea3c7e7c4f399df4b10428791cd5a9ec05b6a
jimatas <atask@hr.nl>
2022-06-03 10:46:28 +0200

    Database record configuration.

---
+++ HR.AnsConnector.Worker/Infrastructure/Persistence/Configurations/StudyRecordConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using HR.AnsConnector.Features.Studies;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HR.AnsConnector.Infrastructure.Persistence.Configurations
+{
+    internal class StudyRecordConfiguration : IEntityTypeConfiguration<StudyRecord>
+    {
+        public void Configure(EntityTypeBuilder<StudyRecord> builder)
+        {
+            builder.HasNoKey();
+
+            builder.Ignore(s => s.CreatedAt);
+            builder.Ignore(s => s.UpdatedAt);
+
+            builder.Property(s => s.DepartmentId).HasColumnName("department_id").HasConversion<string>();
+            builder.Property(s => s.ExternalId).HasColumnName("external_id");
+            builder.Property(s => s.Id).HasColumnName("SyncExternalKey").HasConversion<string>();
+            builder.Property(s => s.EventId).HasColumnName("SyncEventId");
+            builder.Property(s => s.Action).HasColumnName("SyncAction");
+        }
+    }
+}
