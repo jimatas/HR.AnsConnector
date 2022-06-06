b2867daddc54366d56f4d166a38b6b296966b5ed
jimatas <atask@hr.nl>
2022-06-06 13:44:55 +0200

    Slightly more configurable, and logged, retry attempts.

--- HR.AnsConnector.Worker/Features/Common/Commands/MarkAsHandledWithRetry.cs
+++ HR.AnsConnector.Worker/Features/Common/Commands/MarkAsHandledWithRetry.cs
@@ -6,6 +6,9 @@ namespace HR.AnsConnector.Features.Common.Commands
 {
     public class MarkAsHandledWithRetry : ICommandHandlerWrapper<MarkAsHandled>
     {
+        private const int RetryCount = 4;
+        private const int RetryDelayInSeconds = 15;
+
         private readonly ILogger<MarkAsHandledWithRetry> logger;
 
         public MarkAsHandledWithRetry(ILogger<MarkAsHandledWithRetry> logger)
@@ -15,19 +18,22 @@ namespace HR.AnsConnector.Features.Common.Commands
 
         public async Task HandleAsync(MarkAsHandled command, HandlerDelegate next, CancellationToken cancellationToken)
         {
-            await HandleWithRetryAsync(command, next, retries: 5).WithoutCapturingContext();
+            await HandleWithRetryAsync(command, next, tryCount: RetryCount + 1, cancellationToken).WithoutCapturingContext();
         }
 
-        private async Task HandleWithRetryAsync(MarkAsHandled command, HandlerDelegate next, int retries)
+        private async Task HandleWithRetryAsync(MarkAsHandled command, HandlerDelegate next, int tryCount, CancellationToken cancellationToken)
         {
             try
             {
                 await next().WithoutCapturingContext();
             }
-            catch (Exception ex) when (IsTimeoutException(ex) && retries > 0)
+            catch (Exception ex) when (IsTimeoutException(ex) && tryCount > 0)
             {
-                logger.LogWarning($"Timeout expired waiting for {nameof(MarkAsHandled)} call. Attempting retry.");
-                await HandleWithRetryAsync(command, next, retries - 1).WithoutCapturingContext();
+                logger.LogWarning("Timeout expired waiting for {CommandName} command to finish. " +
+                    "Attempting retry in {RetryDelay} secs.", nameof(MarkAsHandled), RetryDelayInSeconds);
+
+                await Task.Delay(TimeSpan.FromSeconds(RetryDelayInSeconds), cancellationToken).WithoutCapturingContext();
+                await HandleWithRetryAsync(command, next, tryCount - 1, cancellationToken).WithoutCapturingContext();
             }
         }
 
